3b6598b0195d6100055ce2ef58e165192775d323
gabrielNunesKlein <gabrielgnk@outlook.com>
2021-08-18 18:03:05 -0300

    Criando a pagina de detalhe da noticia

---
+++ app/routs/noticia.js
@@ -0,0 +1,9 @@
+module.exports = function(app){
+
+    app.get('/noticia', function(req, res){
+        var connection = app.config.dbConnection();
+        connection.query('select * from noticias where id_noticia = 1', function(erro, result){
+            res.render('noticias/noticia', {noticia: result})
+        });
+    });
+}
